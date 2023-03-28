582ba8add357812fcdc20cd8e334d22c775977d9
Teun <teunkoreman@gmail.com>
2023-03-28 20:10:36 +0200

    css

--- resources/views/components/text-input-suffix.blade.php
+++ resources/views/components/text-input-suffix.blade.php
@@ -7,5 +7,5 @@
     <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
         <span class="text-gray-500 sm:text-sm">{{ '@' . $suffix }}</span>
     </div>
-    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full border-gray-300 pr-36 sm:pr-34 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'block w-full border-emerald-300 pr-36 sm:pr-34 focus:border-pink-500 focus:ring-pink-500 rounded-md shadow-sm']) !!}>
 </div>
